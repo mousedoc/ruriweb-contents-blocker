cc05c9b98bb97f52d6e0ec4b0b7d29094ffd19c3
mousedoc <sangyun3631@gmail.com>
2019-09-16 13:56:04 +0900

    Pretty~

---
+++ core/popup.js
@@ -0,0 +1,26 @@
+; (function () {
+    console.log('[Ruriweb-comment-blocker] Running extension')
+
+    BlockComments()
+
+    function BlockComments() {
+        console.log('[Ruriweb-comment-blocker] blockComments()')
+
+        // Add padding-bottom on main_view
+        var board_main_view = document.querySelector('.board_main_view')
+        if (board_main_view) {
+            board_main_view.style.paddingBottom = '20px'
+        }
+        // Remove board_main_bottom
+        var board_main_bottom = document.querySelector('.board_main_bottom')
+        if (board_main_bottom) {
+            board_main_bottom.remove()
+        }
+
+        // Remove comments
+        var board_bottom = document.querySelector('.board_bottom')
+        if (board_bottom) {
+            board_bottom.remove()
+        }
+    }
+})()
